ef2402ac928a262f033fa52d7ee34d418d435650
Sascha Schirra <sash@scoding.de>
2015-08-25 13:46:14 +0200

    refactoring

--- ropperapp/search/search.py
+++ ropperapp/search/search.py
@@ -59,25 +59,23 @@ class Searcher(object):
         filter = self.prepareFilter(filter)
         filtered = {}
         count = 0
-        max_count = 0
-        for g in gadgets.values():
-            max_count += len(g)
-        for section, gadget in gadgets.items():
-            fg = []
-            for g in gadget:
-                if not g.match(filter):
-                    if quality:
-                        if len(g) <= quality+1:
-                            fg.append(g)
-                    else:
+        max_count = len(gadgets)
+        
+        fg = []
+        for g in gadgets:
+            if not g.match(filter):
+                if quality:
+                    if len(g) <= quality+1:
                         fg.append(g)
-                count += 1
-                if pprinter:
-                    pprinter.printProgress('filtering gadgets...', float(count) / max_count)
-            filtered[section] = fg
+                else:
+                    fg.append(g)
+            count += 1
+            if pprinter:
+                pprinter.printProgress('filtering gadgets...', float(count) / max_count)
+            
         if pprinter:
             pprinter.finishProgress();
-        return filtered
+        return fg
 
 
 class Searcherx86(Searcher):

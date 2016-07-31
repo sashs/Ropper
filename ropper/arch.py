925069321f4f959ca8bc112a5ae78ae2d17c51c8
Sascha Schirra <sashs@scoding.de>
2016-07-31 22:50:07 +0200

    find gadgets that can contain not only one of call, jmp and ret instructions at the same time

--- ropper/arch.py
+++ ropper/arch.py
@@ -159,7 +159,7 @@ class ArchitectureX86(Architecture):
             (b'\xff[\x90\x91\x92\x93\x94\x96\x97][\x00-\x0ff]{4}', 6)]
 
     def _initBadInstructions(self):
-        self._badInstructions = ['retf','enter','loop','loopne','int3', 'db', 'ret', 'call', 'jmp']
+        self._badInstructions = ['retf','enter','loop','loopne','int3', 'db']
 
     def _initCategories(self):
         self._categories = {

d89d42ad785bac3d09451e9bb7a1b60a709363d8
Sascha Schirra <sashs@scoding.de>
2016-09-18 15:22:55 +0200

    print address when printing sections of elf files

--- ropper/printer/elfprinter.py
+++ ropper/printer/elfprinter.py
@@ -68,11 +68,13 @@ class ELFPrinter(FileDataPrinter):
         for index in range(len(binary._binary.sections)):
             data.append((cstr('[%.2d]' % index, Color.BLUE),
                         cstr(binary._binary.sections[index].name, Color.WHITE),
+                        cstr(toHex(binary._binary.sections[index].header.sh_addr), Color.GREEN),
                         cstr(elf.SHT[binary._binary.sections[index].header.sh_type], Color.YELLOW)))
 
         self._printTable('Sections',
                         (cstr('Nr', Color.LIGHT_GRAY),
                             cstr('Name', Color.LIGHT_GRAY),
+                            cstr('Address', Color.LIGHT_GRAY),
                             cstr('Type', Color.LIGHT_GRAY)),
                         data)
 

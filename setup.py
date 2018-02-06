5903c3ad4e6bf5ae64edcb94c5368a1007760b6a
Sascha Schirra <sashs@scoding.de>
2018-02-06 11:44:18 +0100

    version changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '1.11.5'
+version = '1.11.6'
 package_name = "ropper"
 package_dir = "ropper"
 package_description = """Show information about files in different file formats and find gadgets to build rop chains for different architectures.

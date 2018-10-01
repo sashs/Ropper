2733d9c8dbdc6bf46168fceba9c1dc3dd9efb159
Sascha Schirra <sash@scoding.de>
2018-10-01 09:56:03 +0200

    removed use of __import__

--- Ropper2.py
+++ Ropper2.py
@@ -17,8 +17,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import ropper
 import sys
 sys.path.append("filebytes")
 
-__import__('ropper').start(sys.argv[1:])
+ropper.start(sys.argv[1:])
+

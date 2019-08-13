5cb1e8cefa46378bd104589ccbeec93a031dedc4
Sascha Schirra <sashs@scoding.de>
2019-08-13 19:30:54 +0200

    removed tests for console

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from testcases.test_arm import *
 from testcases.test_arm64 import *
 from testcases.test_mips import *
 from testcases.test_ppc import *
-from testcases.test_console import *
 import unittest
 
 unittest.main()

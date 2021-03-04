12ce6fc809f9ee7b2024cc4b4b8f8002bf90b5c6
jr <jacob.robie@gmail.com>
2021-03-03 21:17:24 -0500

    fix speed issue with with method of detecting mid gadget ROP instructions

--- ropper/rop.py
+++ ropper/rop.py
@@ -454,22 +454,23 @@ class Ropper(object):
     def __createGadget(self, arch, code_str, codeStartAddress, ending, binary=None, section=None):
         gadget = Gadget(binary, section, arch)
         hasret = False
-        if codeStartAddress == 0x0000000001b34d74:
-            print("found")
         disassembler = self.__getCs(arch)
-
-        for i in disassembler.disasm(code_str, codeStartAddress):
-            if re.match(ending[0], i.bytes):
+        
+        for inst in disassembler.disasm(code_str, codeStartAddress+gadget.imageBase):
+            
+            if re.match(ending[0], inst.bytes):
                 hasret = True
-
-            if hasret or i.mnemonic not in arch.badInstructions:
-                gadget.append(
-                    i.address, i.mnemonic,i.op_str, bytes=i.bytes)
+            
+            if hasret or inst.mnemonic not in arch.badInstructions:
+                gadget.append(inst.address-gadget.imageBase, inst.mnemonic,inst.op_str, bytes=inst.bytes)
 
             if (hasret and not arch.hasBranchDelaySlot) or i.mnemonic in arch.badInstructions:
                 break
-
-
+            
+            for ending2 in arch.endings_re[GadgetType.ROP]:
+                if ending2[0].match(inst.bytes):
+                    # anything before this (including this instr) isn't part of this gadget, go back and try again
+                    return None, -1
 
         leng = len(gadget)
         if hasret and leng > 0:

a5a4de29b88b0cb33067eb5d9c310b8242fffa90
Sascha Schirra <sascha@recurity-labs.com>
2024-05-02 23:17:52 +0200

    version changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '1.13.8'
+version = '1.13.9'
 package_name = "ropper"
 package_dir = "ropper"
 package_description = """Show information about files in different file formats and find gadgets to build rop chains for different architectures.
